68467f8052d3ceff13656d6b6326995562331ac7
ProjectPith <hkmartin08@gmail.com>
2026-09-12 16:09:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ const productsData = [
         image: 'images/laptop-sleeve.jpg', 
         desc: 'fleece interior. YKK 5 nylon zipper. Lightweight.',
         buyUrl: 'YOUR_PRINTIFY_OR_STRIPE_LINK' 
-    }
+    },
     { 
         id: 'p5', 
         title: 'LunarCraft Protective Laptop Sleeve', 
@@ -147,7 +147,7 @@ const productsData = [
         image: 'images/place-holder.jpg', 
         desc: 'Need a custom E-commerce web build for your brand? Reach out for a consultation.',
         buyUrl: 'YOUR_PRINTIFY_OR_STRIPE_LINK' 
-    }
+    },
 ];
 
 function renderProducts() {
