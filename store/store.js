ee6186bd6563f5eac52621728fd2f67e5349b17c
ProjectPith <hkmartin08@gmail.com>
2026-09-14 06:56:37 -0500

    Update store.js

--- store/store.js
+++ store/store.js
@@ -52,8 +52,8 @@ function renderProducts() {
 
   let html = '';
 
-  for (let i = 0; i < productsData.length; i++) {
-    const prod = productsData[i];
+  for (let i = 0; i < products.length; i++) {
+    const prod = products[i];
 
     if (prod.isDemo) {
       html += `
@@ -80,7 +80,7 @@ function renderProducts() {
             <p>${prod.desc}</p>
             <div class="product-price">$${prod.price}</div>
             <div class="card-actions">
-              <button class="btn btn-secondary" onclick="addToCart('${prod.id}')">Test Cart</button>
+              <button class="btn btn-secondary" onclick="addToCart('${product.id}')">Test Cart</button>
               <a href="${prod.buyUrl}" target="_blank" class="btn btn-primary">Buy Now</a>
             </div>
           </div>
