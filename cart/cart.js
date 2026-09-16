0e5a34d42f798a8b02bcb08f46b9f547aa6fc40b
ProjectPith <hkmartin08@gmail.com>
2026-09-15 19:52:38 -0500

    Update cart.js

--- cart/cart.js
+++ cart/cart.js
@@ -142,7 +142,7 @@ async function handleCheckout(event) {
   }
 
   try {
-    const response = await fetch('/API/create-checkout-session', {
+    const response = await fetch('/create-checkout-session', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ items: cart }),
