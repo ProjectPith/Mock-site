0df0787f67f32d4ddecc3ce2f9859a9b9be5db15
ProjectPith <hkmartin08@gmail.com>
2026-09-22 10:59:24 -0500

    Update account.js

--- account/account.js
+++ account/account.js
@@ -8,7 +8,7 @@
   function getSupabase() {
     if (!window.supabaseClient && window.supabase) {
       const SUPABASE_URL = "https://rpfclpfipqspbdbanobj.supabase.co";
-      const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwZmNscGZpcHFzcGJkYmFub2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2MjMwNDMsImV4cCI6MjEwNTE5OTA0M30.I9oy9CDFsEPdPuq2hA6pgnhI79_m4JxsROTfAh4Jjf0";
+      const SUPABASE_ANON_KEY = "sb_publishable_bT739cvrORLIrJYQmUVO2Q_9qe25hOU";
       window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
     }
     return window.supabaseClient;
